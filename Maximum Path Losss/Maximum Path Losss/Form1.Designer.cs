8797688f0af20d4389c33764799d28c0f634a554
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2022-09-25 22:12:16 +0330

    Add files via upload

---
+++ Maximum Path Losss/Maximum Path Losss/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Maximum_Path_Losss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TP = new System.Windows.Forms.TextBox();
+            this.RS = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TAG = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FM = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.MPL = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.MPL1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.F = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.D = new System.Windows.Forms.Label();
+            this.MPL2 = new System.Windows.Forms.Button();
+            this.D1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TP
+            // 
+            this.TP.Location = new System.Drawing.Point(112, 44);
+            this.TP.Name = "TP";
+            this.TP.Size = new System.Drawing.Size(100, 20);
+            this.TP.TabIndex = 0;
+            // 
+            // RS
+            // 
+            this.RS.Location = new System.Drawing.Point(112, 116);
+            this.RS.Name = "RS";
+            this.RS.Size = new System.Drawing.Size(100, 20);
+            this.RS.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Transmit Power";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Receiver Sensitivity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(331, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Receive Antenna Gain";
+            // 
+            // RA
+            // 
+            this.RA.Location = new System.Drawing.Point(452, 44);
+            this.RA.Name = "RA";
+            this.RA.Size = new System.Drawing.Size(100, 20);
+            this.RA.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(331, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Transmit Antenna Gain";
+            // 
+            // TAG
+            // 
+            this.TAG.Location = new System.Drawing.Point(452, 109);
+            this.TAG.Name = "TAG";
+            this.TAG.Size = new System.Drawing.Size(100, 20);
+            this.TAG.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "The Fade Margin";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // FM
+            // 
+            this.FM.Location = new System.Drawing.Point(112, 180);
+            this.FM.Name = "FM";
+            this.FM.Size = new System.Drawing.Size(100, 20);
+            this.FM.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(345, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Maximum Path Loss :";
+            // 
+            // MPL
+            // 
+            this.MPL.AutoSize = true;
+            this.MPL.Location = new System.Drawing.Point(452, 183);
+            this.MPL.Name = "MPL";
+            this.MPL.Size = new System.Drawing.Size(0, 13);
+            this.MPL.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 246);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Maximum Path Loss";
+            // 
+            // MPL1
+            // 
+            this.MPL1.Location = new System.Drawing.Point(113, 246);
+            this.MPL1.Name = "MPL1";
+            this.MPL1.Size = new System.Drawing.Size(100, 20);
+            this.MPL1.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 305);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Frequency(MHz)";
+            // 
+            // F
+            // 
+            this.F.Location = new System.Drawing.Point(109, 305);
+            this.F.Name = "F";
+            this.F.Size = new System.Drawing.Size(104, 20);
+            this.F.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(397, 279);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Distance :";
+            // 
+            // D
+            // 
+            this.D.AutoSize = true;
+            this.D.Location = new System.Drawing.Point(459, 279);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(0, 13);
+            this.D.TabIndex = 17;
+            // 
+            // MPL2
+            // 
+            this.MPL2.Location = new System.Drawing.Point(161, 363);
+            this.MPL2.Name = "MPL2";
+            this.MPL2.Size = new System.Drawing.Size(120, 44);
+            this.MPL2.TabIndex = 18;
+            this.MPL2.Text = "Maximum Path Loss";
+            this.MPL2.UseVisualStyleBackColor = true;
+            this.MPL2.Click += new System.EventHandler(this.MPL2_Click);
+            // 
+            // D1
+            // 
+            this.D1.Location = new System.Drawing.Point(341, 363);
+            this.D1.Name = "D1";
+            this.D1.Size = new System.Drawing.Size(118, 44);
+            this.D1.TabIndex = 19;
+            this.D1.Text = "Distance";
+            this.D1.UseVisualStyleBackColor = true;
+            this.D1.Click += new System.EventHandler(this.D1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 450);
+            this.Controls.Add(this.D1);
+            this.Controls.Add(this.MPL2);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.F);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.MPL1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.MPL);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.FM);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TAG);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RS);
+            this.Controls.Add(this.TP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Maximum Path Loss And Distance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TP;
+        private System.Windows.Forms.TextBox RS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox RA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TAG;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox FM;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label MPL;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox MPL1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox F;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label D;
+        private System.Windows.Forms.Button MPL2;
+        private System.Windows.Forms.Button D1;
+    }
+}
+
---
+++ Maximum Path Losss/Maximum Path Losss/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maximum_Path_Losss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MPL2_Click(object sender, EventArgs e)
+        {
+            double tp = Double.Parse(TP.Text);
+            double rs = Double.Parse(RS.Text);
+            double ra = Double.Parse(RA.Text);
+            double tag = Double.Parse(TAG.Text);
+            double fm = Double.Parse(FM.Text);
+            double g = ra + tag;
+            double mpl = tp - rs + g - fm;
+            MPL.Text = (mpl).ToString();
+           
+        }
+
+        private void D1_Click(object sender, EventArgs e)
+        {
+            double mpl3 = Double.Parse(MPL1.Text);
+            double f = Double.Parse(F.Text);
+            double X = (mpl3 - 32.44 - Math.Log(f)) / 20;
+            double Y = Math.Pow(10, X);
+            D.Text = (Y).ToString();
+        }
+    }
+}
